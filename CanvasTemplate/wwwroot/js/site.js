0b2515b3c77a03bdc9106b58d1fe3dcb56a09af1
Eric Ung <ungericw@tutanota.com>
2023-07-09 22:43:00 -0500

    add rectangles

--- CanvasTemplate/wwwroot/js/site.js
+++ CanvasTemplate/wwwroot/js/site.js
@@ -1,14 +1,41 @@
 ﻿var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
+var x = 0;
+var y = 0;
+var message = "";
+var rects = [];
 
 canvas.addEventListener('mousemove', function (evt) {
     var mousePos = getMousePos(canvas, evt);
-    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
 
+    draw();
+}, false);
+
+canvas.addEventListener('mousedown', function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    x = mousePos.x;
+    y = mousePos.y;
+}, false);
+
+canvas.addEventListener('mouseup', function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    rects.push({ x: x, y: y, w: mousePos.x - x, h: mousePos.y - y });
+    x = mousePos.x;
+    y = mousePos.y;
+    message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    draw();
+}, false);
+
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillText(message, 10, 50);
-}, false);
+
+    for (var i = 0; i < rects.length; i++) {
+        ctx.fillRect(rects[i].x, rects[i].y, rects[i].w, rects[i].h);
+    }
+}
 
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
