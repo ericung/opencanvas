d4a5081f38ab4e5037a8a0f97f9616b714995609
Eric Ung <ungericw@tutanota.com>
2023-09-30 19:41:53 -0500

    Add radio functionality

---
+++ OpenCanvas/Models/RadioButtonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenCanvas.Models
+{
+    public class RadioButtonModel
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double w { get; set; }
+        public double h { get; set; }
+        public int id { get; set; }
+
+    }
+}
